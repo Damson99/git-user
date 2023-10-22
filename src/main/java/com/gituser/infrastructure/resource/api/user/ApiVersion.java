78492312b026bdd657fa3b0ddaaee5b6bec821de
damson99 <damian.wieczorek99@gmail.com>
2023-10-23 01:05:00 +0200

    GU02-api-version-fix changing api version's name

--- src/main/java/com/gituser/infrastructure/resource/api/user/ApiVersion.java
+++ src/main/java/com/gituser/infrastructure/resource/api/user/ApiVersion.java
@@ -5,5 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ApiVersion {
 
-    public static final String V1_JSON = "application/vnd.dispatcher.v1+json";
+    public static final String V1_JSON = "application/vnd.gituser.v1+json";
 }
